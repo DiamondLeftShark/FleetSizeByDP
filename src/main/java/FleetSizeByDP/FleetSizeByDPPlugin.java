b669dfa7d0ae5cb02c72952ab959f1d30d67e75c
Chozo <youdarerefuse@mybatchall.com>
2021-06-15 22:25:15 -0700

    Begin building out FleetSizeByDPPlugin.java

--- src/main/java/FleetSizeByDP/FleetSizeByDPPlugin.java
+++ src/main/java/FleetSizeByDP/FleetSizeByDPPlugin.java
@@ -1,5 +1,15 @@
 package FleetSizeByDP;
 
-public class FleetSizeByDPPlugin {
+import com.fs.starfarer.api.BaseModPlugin;
+import com.fs.starfarer.api.Global;
+
+public class FleetSizeByDPPlugin extends BaseModPlugin {
     
+
+    @Override
+    public void onGameLoad(boolean newGame) {
+        //set player fleet to large number of max ships
+        //currently set to 10 for testing
+        Global.getSettings().setFloat("maxShipsInFleet", 10f);
+    }
 }
