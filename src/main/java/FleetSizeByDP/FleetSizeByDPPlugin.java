057ce4b12e204ed96f4b546353b3b8577c37502f
Chozo <youdarerefuse@mybatchall.com>
2021-06-15 23:03:05 -0700

    Added data\config\settings.json file

--- src/main/java/FleetSizeByDP/FleetSizeByDPPlugin.java
+++ src/main/java/FleetSizeByDP/FleetSizeByDPPlugin.java
@@ -4,12 +4,10 @@ import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.Global;
 
 public class FleetSizeByDPPlugin extends BaseModPlugin {
-    
 
     @Override
     public void onGameLoad(boolean newGame) {
-        //set player fleet to large number of max ships
-        //currently set to 10 for testing
-        Global.getSettings().setFloat("maxShipsInFleet", 10f);
+        int maxFleet = Global.getSettings().getInt("maxShipsInFleet");
+        Global.getSector().getCampaignUI().addMessage("FleetSizeByDP loaded: current max fleet size is " + maxFleet + ".");
     }
 }
