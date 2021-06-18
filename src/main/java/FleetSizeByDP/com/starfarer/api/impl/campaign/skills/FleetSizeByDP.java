da9ac5db6bfc5901bc05195e96537ee4e80b9785
Chozo <35671546+DiamondLeftShark@users.noreply.github.com>
2021-06-18 16:58:09 -0700

    Refactor code in FleetSizeByDP

--- src/main/java/FleetSizeByDP/com/starfarer/api/impl/campaign/skills/FleetSizeByDP.java
+++ src/main/java/FleetSizeByDP/com/starfarer/api/impl/campaign/skills/FleetSizeByDP.java
@@ -13,16 +13,26 @@ import com.fs.starfarer.api.combat.MutableShipStatsAPI;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
 import com.fs.starfarer.api.ui.TooltipMakerAPI;
 
-//"Skill" that checks current fleet DP and applies increased supply use/burn speed reduction if over the limit.
+//Skill that checks current player fleet DP and applies increased supply use/burn speed reduction if over the limit.
 //This skill should only ever be assigned to the player character, for obvious reasons.
 public class FleetSizeByDP {
 
+    //settings.json values
     private static int MAX_FLEET_BY_DP = Global.getSettings().getInt("maxShipsInPlayerFleetByDP");
     private static float SUPPLY_PENALTY_MULT = Global.getSettings().getFloat("suppliesPerShipOverMaxInFleet");
-    //public static boolean ENABLED = false;
     
-    //Supply multiplier
+    //Supply multiplier id
     public static String SUPPLIES_BY_FLEET_DP = "supply_use_mult_by_dp";
+    //burn multiplier id
+    public static String BURN_MULT_BY_FLEET_DP = "burn_mult_by_dp";
+
+    //helper function for current OP
+    public static float getCurrentFleetOP() {
+        CampaignFleetAPI fleet = Global.getSector().getPlayerFleet();
+        MutableCharacterStatsAPI stats = Global.getSector().getPlayerStats();
+
+        return BaseSkillEffectDescription.getTotalOP(fleet.getFleetData(), stats);
+    }
 
     //Level1: implements supply use modifier
     public static class Level1 extends BaseSkillEffectDescription implements ShipSkillEffect, FleetTotalSource {
@@ -33,14 +43,12 @@ public class FleetSizeByDP {
 		}
 
         public void apply(MutableShipStatsAPI stats, HullSize hullSize, String id, float level) {
-            //TBD
             id = SUPPLIES_BY_FLEET_DP;
             float useMult = getFleetSupplyMult();
             stats.getSuppliesPerMonth().modifyMult(id, useMult);
         }
 
         public void unapply(MutableShipStatsAPI stats, HullSize hullSize, String id) {
-            //TBD
             id = SUPPLIES_BY_FLEET_DP;
             stats.getSuppliesPerMonth().unmodify(id);
         }
@@ -54,12 +62,10 @@ public class FleetSizeByDP {
 		}
 
         //calculate fleet supply multiplier
-        private float getFleetSupplyMult() {
+        public float getFleetSupplyMult() {
             //public static float getTotalOP(FleetDataAPI data, MutableCharacterStatsAPI stats)
-            CampaignFleetAPI fleet = Global.getSector().getPlayerFleet();
-		    MutableCharacterStatsAPI stats = Global.getSector().getPlayerStats();
 
-            float currentOP = getTotalOP(fleet.getFleetData(), stats);
+            float currentOP = getCurrentFleetOP();
 
             if(currentOP <= MAX_FLEET_BY_DP) {
                 return 1f;
@@ -73,9 +79,33 @@ public class FleetSizeByDP {
     }
 
     //Level 2: implements burn speed multiplier
-/*    
-    public static class Level2 extends BaseSkillEffectDescription implements ShipSkillEffect, FleetTotalSource {
-        
-    } 
-*/
+   
+    // public static class Level2 extends BaseSkillEffectDescription implements ShipSkillEffect, FleetTotalSource {
+    //     public FleetTotalItem getFleetTotalItem() {
+	// 		return getOPTotal();
+	// 	}
+
+    //     public void apply(MutableShipStatsAPI stats, HullSize hullSize, String id, float level) {
+    //         //TBD
+    //     }
+
+    //     public void unapply(MutableShipStatsAPI stats, HullSize hullSize, String id) {
+    //         //TBD
+    //     }
+
+    //     public String getEffectDescription(float level) {
+    //         return null;
+    //     }
+
+    //     public ScopeDescription getScopeDescription() {
+	// 		return ScopeDescription.FLEET;
+	// 	}
+
+    //     public float getFleetBurnMult() {
+
+    //         return 1f;
+    //     }
+
+    // } 
+
 }
