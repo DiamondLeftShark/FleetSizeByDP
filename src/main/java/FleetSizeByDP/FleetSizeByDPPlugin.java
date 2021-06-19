f992b9321ba3d8f34fd4f303334c6e9c212c4ccf
Chozo <35671546+DiamondLeftShark@users.noreply.github.com>
2021-06-18 20:05:08 -0700

    Additional work on removing stat modifiers when disabled

--- src/main/java/FleetSizeByDP/FleetSizeByDPPlugin.java
+++ src/main/java/FleetSizeByDP/FleetSizeByDPPlugin.java
@@ -1,7 +1,9 @@
 package FleetSizeByDP;
 
+import java.util.List;
 import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.fleet.FleetMemberAPI;
 import FleetSizeByDP.com.starfarer.api.impl.campaign.skills.FleetSizeByDP;
 
 public class FleetSizeByDPPlugin extends BaseModPlugin {
@@ -11,18 +13,21 @@ public class FleetSizeByDPPlugin extends BaseModPlugin {
     @Override
     public void onGameLoad(boolean newGame) {
 
-        //Add skill to player character on game load if enabled: else, set skill level to 0
+        //Add skill to player character on game load if enabled
         if(MOD_ENABLED) {
             Global.getSector().getPlayerPerson().getStats().setSkillLevel("fleet_size_by_dp", 1);
         } 
+        //if disabled, set skill to 0 and remove existing modifiers
         else {
-            //Global.getSector().getPlayerPerson().getStats().setSkillLevel("fleet_size_by_dp", 0);
-            //remove existing modifiers, if present
-            //getSuppliesPerMonth().unmodify(FleetSizeByDP.SUPPLIES_BY_FLEET_DP);
-            //Global.getSector().getPlayerFleet().getStats().getFleetwideMaxBurnMod().unmodifyMult(FleetSizeByDP.BURN_MULT_BY_FLEET_DP);
-            if(Global.getSector().hasScript(FleetSizeByDP.class)) {
-                Global.getSector().removeScriptsOfClass(FleetSizeByDP.class)
+            Global.getSector().getPlayerPerson().getStats().setSkillLevel("fleet_size_by_dp", 0);
+
+            List<FleetMemberAPI> playerFleet = Global.getSector().getPlayerFleet().getFleetData().getMembersInPriorityOrder();
+            
+            for(FleetMemberAPI ship : playerFleet) {
+                ship.getStats().getSuppliesPerMonth().unmodify(FleetSizeByDP.SUPPLIES_BY_FLEET_DP);
             }
+           
+            Global.getSector().getPlayerFleet().getStats().getFleetwideMaxBurnMod().unmodifyMult(FleetSizeByDP.BURN_MULT_BY_FLEET_DP);
         }
 
     }
