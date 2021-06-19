01dc1d52f50c6cf581bc430deb6aa7115872215b
Chozo <35671546+DiamondLeftShark@users.noreply.github.com>
2021-06-18 17:14:17 -0700

    Initial implementation of burn speed penalty

--- src/main/java/FleetSizeByDP/com/starfarer/api/impl/campaign/skills/FleetSizeByDP.java
+++ src/main/java/FleetSizeByDP/com/starfarer/api/impl/campaign/skills/FleetSizeByDP.java
@@ -21,20 +21,20 @@ public class FleetSizeByDP {
     private static int MAX_FLEET_BY_DP = Global.getSettings().getInt("maxShipsInPlayerFleetByDP");
     private static float SUPPLY_PENALTY_MULT = Global.getSettings().getFloat("suppliesPerShipOverMaxInFleet");
     
-    //Supply multiplier id
-    public static String SUPPLIES_BY_FLEET_DP = "supply_use_mult_by_dp";
-    //burn multiplier id
-    public static String BURN_MULT_BY_FLEET_DP = "burn_mult_by_dp";
-
-    //helper function for current OP
-    public static float getCurrentFleetOP() {
+    //helper function for current fleet DP
+    public static float getCurrentFleetDP() {
         CampaignFleetAPI fleet = Global.getSector().getPlayerFleet();
         MutableCharacterStatsAPI stats = Global.getSector().getPlayerStats();
 
         return BaseSkillEffectDescription.getTotalOP(fleet.getFleetData(), stats);
     }
 
+
     //Level1: implements supply use modifier
+
+    //Supply multiplier id
+    public static String SUPPLIES_BY_FLEET_DP = "supply_use_mult_by_dp";
+
     public static class Level1 extends BaseSkillEffectDescription implements ShipSkillEffect, FleetTotalSource {
 
         //display OP total on fleet page
@@ -63,49 +63,62 @@ public class FleetSizeByDP {
 
         //calculate fleet supply multiplier
         public float getFleetSupplyMult() {
-            //public static float getTotalOP(FleetDataAPI data, MutableCharacterStatsAPI stats)
 
-            float currentOP = getCurrentFleetOP();
+            float currentDP = getCurrentFleetDP();
 
-            if(currentOP <= MAX_FLEET_BY_DP) {
+            if(currentDP <= MAX_FLEET_BY_DP) {
                 return 1f;
             } 
             else {
                 float baseMult = 1 + Math.max(SUPPLY_PENALTY_MULT, 0);
-                float fleetOverMult = (currentOP / MAX_FLEET_BY_DP);
+                float fleetOverMult = (currentDP / MAX_FLEET_BY_DP);
                 return baseMult * fleetOverMult;
             }
         }
     }
 
     //Level 2: implements burn speed multiplier
+
+    //burn multiplier id
+    public static String BURN_MULT_BY_FLEET_DP = "burn_mult_by_dp";
    
-    // public static class Level2 extends BaseSkillEffectDescription implements ShipSkillEffect, FleetTotalSource {
-    //     public FleetTotalItem getFleetTotalItem() {
-	// 		return getOPTotal();
-	// 	}
+    public static class Level2 extends BaseSkillEffectDescription implements ShipSkillEffect, FleetTotalSource {
+        public FleetTotalItem getFleetTotalItem() {
+			return getOPTotal();
+		}
 
-    //     public void apply(MutableShipStatsAPI stats, HullSize hullSize, String id, float level) {
-    //         //TBD
-    //     }
+        public void apply(MutableShipStatsAPI stats, HullSize hullSize, String id, float level) {
+            //TBD
+            id = BURN_MULT_BY_FLEET_DP;
+            float burnMult = getFleetBurnMult();
+            Global.getSector().getPlayerFleet().getStats().getFleetwideMaxBurnMod().modifyMult(id, burnMult);
+        }
 
-    //     public void unapply(MutableShipStatsAPI stats, HullSize hullSize, String id) {
-    //         //TBD
-    //     }
+        public void unapply(MutableShipStatsAPI stats, HullSize hullSize, String id) {
+            //TBD
+            id = BURN_MULT_BY_FLEET_DP;
+            Global.getSector().getPlayerFleet().getStats().getFleetwideMaxBurnMod().unmodifyMult(id);
+        }
 
-    //     public String getEffectDescription(float level) {
-    //         return null;
-    //     }
+        public String getEffectDescription(float level) {
+            return null;
+        }
 
-    //     public ScopeDescription getScopeDescription() {
-	// 		return ScopeDescription.FLEET;
-	// 	}
+        public ScopeDescription getScopeDescription() {
+			return ScopeDescription.FLEET;
+		}
 
-    //     public float getFleetBurnMult() {
+        public float getFleetBurnMult() {
+            float currentDP = getCurrentFleetDP();
 
-    //         return 1f;
-    //     }
+            if(currentDP <= MAX_FLEET_BY_DP) {
+                return 1f;
+            } 
+            else {
+                return 0.5f;
+            }
+        }
 
-    // } 
+    } 
 
 }
