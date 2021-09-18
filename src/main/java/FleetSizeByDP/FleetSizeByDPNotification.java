f1c32ca9545a9a80dfff5a0c2babd6812c7d4d9c
Chozo <35671546+DiamondLeftShark@users.noreply.github.com>
2021-09-18 13:59:54 -0700

    Add framework and pseudocode to FleetSizeByDPNotification

--- src/main/java/FleetSizeByDP/FleetSizeByDPNotification.java
+++ src/main/java/FleetSizeByDP/FleetSizeByDPNotification.java
@@ -1,5 +1,39 @@
 package FleetSizeByDP;
 
-public class FleetSizeByDPNotification {
+import com.fs.starfarer.api.EveryFrameScript;
+import com.fs.starfarer.api.Global;
+
+import java.awt.Color;
+
+public class FleetSizeByDPNotification implements EveryFrameScript {
+
+    private float CHECK_INTERVAL = 10f;
+
+    private float secSinceLastCheck = 0f;
     
+    @Override
+    public boolean isDone() {
+        return false;
+    }
+    
+    @Override
+    public boolean runWhilePaused() {
+        return false;
+    }
+
+    @Override
+    public void advance(float amount) {
+        //add amount to secSinceLastCheck
+        //if secSinceLastCheck >= check_interval:
+        //if player is under fleet limit, do nothing
+        //otherwise, send notification to player while on campaign map
+        //reset secSinceLastCheck to 0 afterwards
+        secSinceLastCheck += amount;
+
+        if(secSinceLastCheck >= CHECK_INTERVAL) {
+            //notification goes here
+
+            secSinceLastCheck = 0f;
+        }
+    }
 }
