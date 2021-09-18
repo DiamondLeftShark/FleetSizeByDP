cb4132761aa532c6ddc68033cb945816cdee6316
Chozo <35671546+DiamondLeftShark@users.noreply.github.com>
2021-09-18 15:45:24 -0700

    Troubleshooting issues with test notification

--- src/main/java/FleetSizeByDP/FleetSizeByDPNotification.java
+++ src/main/java/FleetSizeByDP/FleetSizeByDPNotification.java
@@ -2,7 +2,6 @@ package FleetSizeByDP;
 
 import com.fs.starfarer.api.EveryFrameScript;
 import com.fs.starfarer.api.Global;
-import com.fs.starfarer.api.campaign.CampaignUIAPI;
 
 import java.awt.Color;
 
